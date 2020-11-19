4d0be0f063837d15038508002a8608d5711dbd8c
byron <zhibohi@163.com>
2020-11-19 15:34:07 +0800

    1) Setup config 2) setup app 2) setup manager
    Signed-off-by: byron <zhibohi@163.com>

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @File: __init__.py.py
+# @Author: byron
+# @Date: 11/19/20
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+def create_app(config_name):
+    pass
+
--- app/utils/setup_log.py
+++ app/utils/setup_log.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# @File: __init__.py.py
+# @File: setup_log.py
 # @Author: byron
 # @Date: 11/19/20
